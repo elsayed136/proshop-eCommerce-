c741b6c072bc7e9843a38a39272c87f0d6e42658
elsayed136 <elsayed.ahmed5757@outlook.com>
2021-03-29 09:27:00 +0200

    feat: Adding router to app and not found component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
-
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
+import NotFound from './components/NotFound'
 
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
-        <Container className="fill">
-          <HomePage />
+        <Container>
+          <Switch>
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={HomePage} />
+            <Redirect to="/not-found" />
+          </Switch>
         </Container>
       </main>
       <Footer />
