34b0db9a1851fb2f135ffbc0b6bfe5a2de0799e3
elsayed136 <elsayed.ahmed5757@outlook.com>
2021-03-30 07:01:53 +0200

    feat: Added colors for console

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -23,4 +24,7 @@ app.get('/api/product/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server is running on http://localhost:${PORT}`.yellow.bold)
+)
