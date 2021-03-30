a1f0a38665fda8289f9a98d2000a10252b691427
elsayed136 <elsayed.ahmed5757@outlook.com>
2021-03-30 10:08:35 +0200

    refactor: moving routes in separed folder

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+
+import Product from '../models/productModel.js'
+
+const router = express.Router()
+
+// GET /api/products
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const porducts = await Product.find({})
+    res.json(porducts)
+  })
+)
+
+// GET /api/products/:id
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) res.status(404).json({ message: 'not found' })
+    res.json(product)
+  })
+)
+
+export default router
